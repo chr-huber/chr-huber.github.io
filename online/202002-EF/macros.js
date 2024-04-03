14bf3b28ecf46db2694c073228cb0d35e89def7d
Christoph <chr.huber@outlook.com>
2024-04-03 12:13:30 +0900

    .

--- online/202002-EF/macros.js
+++
@@ -1,9 +0,0 @@
-remark.macros.width = function (percentage) {
-  var url = this;
-  return '<img src="' + url + '" style="width: ' + percentage + '" />';
-};
-
-remark.macros.height = function (percentage) {
-  var url = this;
-  return '<img src="' + url + '" style="height: ' + percentage + '" />';
-};
